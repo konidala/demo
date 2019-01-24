43a19f2694ad950639ad84d47b6b8d64cb11cee9
Konidala, Puneetha <puneetha.konidala@pioneer.com>
2019-01-24 16:29:39 +0530

    Handling of +, - done.

--- script1.py
+++ script1.py
@@ -7,7 +7,6 @@ class GoogleTestCase(unittest.TestCase):
     def testPageTitle(self):
         self.browser = webdriver.Chrome(r"C:/Users/konidapu/Downloads/chromedriver_win32/chromedriver.exe")
         self.browser.implicitly_wait(60)
-        # self.browser = webdriver.Chrome()
         self.addCleanup(self.browser.quit)
         self.browser.get('https://github.com/konidala/demo/commits/master')
         #self.browser.get('https://github.com/konidala/datascience/commits/master')
@@ -18,20 +17,25 @@ class GoogleTestCase(unittest.TestCase):
         older_button = self.browser.find_element_by_xpath("//*[contains(text(),'Older')]").get_attribute('href')
 
         for i in total_commits_per_page:
-            #i.click()
             self.browser.get(i)
             time.sleep(1)
             total_files = self.browser.find_elements_by_xpath("//div[contains(@id, 'diff-')]")
             for file in total_files:
-                commit_author = self.browser.find_element_by_xpath("//a[contains(@class, 'commit-author')]").text
-                print(file.text)
+                commit_author = self.browser.find_element_by_xpath("//*[contains(@class, 'commit-author')]").text
                 with open("result.txt", 'a') as file1:
-                    file1.write(file.text)
-                    file1.write("\n==========================================================\n")
+                    table = file.find_elements_by_xpath("//tbody")
 
-                # committed_files = self.browser.find_elements_by_xpath("//div[contains(@id, 'diff-')]")
-                # for change in committed_files:
-                #     change.find_elements_by_xpath("//table[@class='datadisplaytable']")
+                    for tbody in table:
+                        all_rows = tbody.find_elements_by_xpath(".//tr")
+                        file1.write("Author: -->" + commit_author + "\n")
+                        file1.write("File Name: --> " + self.browser.find_element_by_xpath("//div[contains(@class, 'file-info')]//a").text  + "\n")
+                        for td in all_rows[1:]:
+                            file1.write(td.find_element_by_xpath(".//td[3]").get_attribute("data-code-marker") + td.find_element_by_xpath(".//td[4]").text  + "\n")
+                        file1.write("=============================================================\n")
+
+        element = self.browser.find_element_by_xpath("//div[contains(@class, 'pagination')]//a")
+        if element.is_displayed() and (element.text == 'Older'):
+            pass
 
 
 if __name__ == '__main__':
